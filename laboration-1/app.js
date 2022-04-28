1e55e617a5728a3afeed5e748960c86b1692601d
Huto Friberg <hugofrib100@student.kau.se>
2022-04-28 11:22:23 +0200

    yaas

--- laboration-1/app.js
+++ laboration-1/app.js
@@ -1,5 +1,27 @@
-//Laboration 1
+"Use strict"
 
+//Laboration 1
+const express = require("express");
+const fs = require("fs");
+const jsdom = require("jsdom");
+
+let app = express();
+
+
+
+
+app.get('/', (req, res) => {
+    let indexFile = fs.readFileSync("./index.html", (err, data) => {
+        if (err){ throw err;}
+        console.log(data);
+    });
+
+    let dom = new jsdom.JSDOM(indexFile.toString);
+
+    let sectionTag = dom.window.document.querySelector("section");
+});
+
+app.listen(3000);
 
 
 
